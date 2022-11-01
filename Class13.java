79a6f76ff25ed19295c76ec054048892f82e54a8
LIN115 <113489076+LIN115@users.noreply.github.com>
2022-11-01 21:30:17 +0800

    Update Class13.java

--- Class13.java
+++ Class13.java
@@ -1,37 +1,25 @@
 class CCircle {
-	double area;
-
 	double area(double r) {
-		area = r * r * 3.14;
-		return (double) area;
+		return (3.14 * r * r);
 	}
 
 	float area(float r) {
-		area = r * r * 3.14;
-		return (float) area;
+		return (3.14f * r * r);
 	}
 
-	int area(int r) {
-		area = r * r * 3.14;
-		return (int) area;
+	double area(int r) {
+		return (3.14 * r * r);
 	}
-
-	public void show() {
-		System.out.println("area=" + area);
-	}
-
 }
 
 public class Class13 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		CCircle a = new CCircle();
-		a.area(2);
-		a.show();
-		a.area(2.2f);
-		a.show();
-		a.area(2.2);
-		a.show();
+		CCircle obj = new CCircle();
+
+		System.out.println("area(2)=" + obj.area(2));
+		System.out.println("area(2.2f)=" + obj.area(2.2f));
+		System.out.println("area(2.2)=" + obj.area(2.2));
 	}
 }
